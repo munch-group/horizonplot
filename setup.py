ff976bc74b0f424b28de40ecbcc75272db5f7df4
Kasper Munch <kaspermunch@birc.au.dk>
2019-01-09 13:54:39 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='HorizonPlot',
+      version='1.0',
+      description='Generates horizon plots.',
+      url='https://github.com/kaspermunch/HorizonPlot',
+      author='Kasper Munch',
+      packages=['HorizonPlot'],
+      zip_safe=False)
+
