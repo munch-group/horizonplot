76c7c661adb1444b69a9667c49cdacda9d4b1a16
Kasper Munch Terkelsen <kmt@birc.au.dk>
2020-04-30 23:36:32 +0200

    Setup for conda package.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 
-from setuptools import setup
+import setuptools
 
-setup(name='HorizonPlot',
+setuptools.setup(name='horizonplot',
       version='1.0',
-      description='Generates horizon plots.',
-      url='https://github.com/kaspermunch/HorizonPlot',
       author='Kasper Munch',
-      packages=['HorizonPlot'],
-      zip_safe=False)
-
+      description='Generates horizon plots.',
+      # long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/kaspermunch/horizonplot',
+      packages=setuptools.find_packages(),
+      python_requires='>=3.6',
+      install_requires=[
+      'pandas>=1.0',
+      'numpy>=1.1',
+      'seaborn>=0.1,
+      'matplotlib>=3.2',
+      ])
