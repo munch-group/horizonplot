c0591fcc58f10a5cd1125c4edddc64970d98467a
Kasper Munch Terkelsen <kmt@birc.au.dk>
2020-05-27 08:55:04 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 
 setuptools.setup(name='horizonplot',
-      version='1.0.5',
+      version='1.0.6',
       author='Kasper Munch',
       description='Generates horizon plots.',
       # long_description=long_description,
